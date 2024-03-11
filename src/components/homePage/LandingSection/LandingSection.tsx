5b9541bf2c4fb9dd8555e8243998af06a16cee6d
Vinita Mathew <vinitamathew2010@gmail.com>
2024-03-11 20:26:33 +0530

    styling of landing page

--- src/components/homePage/LandingSection/LandingSection.tsx
+++ src/components/homePage/LandingSection/LandingSection.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-// import TitleIcon from "../../../assets/title-icon.svg";
 import "./LandingSection.scss";
 
-// const { ReactComponent } = require("react-svg-loader");
-const TitleIcon = require("../../../assets/title-icon.svg").default;
+//const TitleIcon = require("../../../assets/title-icon.svg").default;
 
 export default function LandingSection(props: any) {
   return (
     <div className="landing-section">
-      <div className="heading-container">
-        <img className="title-icon" src={TitleIcon} alt=""></img>
-        <div className="line1">Hey there! I’m Vinita Mathew.</div>
-        <div className="line2">
-          I develop websites that are responsive, accessible and efficient.
+      <div className="landing-page-content">
+        <div className="heading-container">
+          Crafting Exceptional User Experiences through Code
         </div>
-      </div>
-      <div className="details-container">
-        <div className="detail-buttons">
-          <button className="work-button" onClick={props.workClick}>
-            View my work
-          </button>
-          <button className="contact-button" onClick={props.contactClick}>
-            Contact
-          </button>
-        </div>
-        <div className="description">
-          I'm a React developer who loves to make things look good and work
-          well. <b>I'm currently at TCS</b>, but I'm always looking for new
-          challenges. If you have a project that needs some front-end love, give
-          me a shout!
+        <div className="details-container">
+          <div className="description">
+            Greetings! I'm Vinita Mathew, a passionate React developer dedicated
+            to crafting visually appealing and seamlessly functional
+            experiences.
+          </div>
         </div>
       </div>
     </div>
