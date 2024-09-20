031235240572eaa5490df1a8ffd8bfdadf7ec16f
Vinita Mathew <vinitamathew2010@gmail.com>
2024-09-20 19:30:35 +0530

    added ecommerce project

--- src/components/homePage/Projects/ProjectsData.ts
+++ src/components/homePage/Projects/ProjectsData.ts
@@ -1,6 +1,7 @@
 const PortfolioImage = require("../../../assets/portfolio.png");
+const ShoppingImage = require("../../../assets/shopping-website.png");
 const TagImage = require("../../../assets/tag.png");
-const BrightSmilesImage = require("../../../assets/bright-smiles.png");
+//const BrightSmilesImage = require("../../../assets/bright-smiles.png");
 
 export const projectsData = [
   {
@@ -11,17 +12,24 @@ export const projectsData = [
     link: "https://nishaduxd.com/",
   },
   {
+    imageUrl: ShoppingImage,
+    title: "Online Shopping",
+    desciption: "E-Commerce website",
+    keyPoints: ["Vite", "Redux", "MaterialUI"],
+    link: "https://github.com/VinitaMathew/ShoppingWebsite",
+  },
+  {
     imageUrl: TagImage,
     title: "TAG - Food and Beverage Solutions",
     desciption: "Enterprise website",
-    keyPoints: ["JavaScript", "ReactJs", "Routing"],
+    keyPoints: ["Jest", "SCSS", "RouterDom"],
     link: "https://vinitamathew.github.io/tag/#/",
   },
-  {
-    imageUrl: BrightSmilesImage,
-    title: "Bright Smiles Orthodontics",
-    desciption: "Dental clinic website",
-    keyPoints: ["SCSS", "Responsive", "TypeScript"],
-    link: "https://vinitamathew.github.io/BrightSmiles/",
-  },
+  // {
+  //   imageUrl: BrightSmilesImage,
+  //   title: "Bright Smiles Orthodontics",
+  //   desciption: "Dental clinic website",
+  //   keyPoints: ["SCSS", "Responsive", "TypeScript"],
+  //   link: "https://vinitamathew.github.io/BrightSmiles/",
+  // },
 ];
